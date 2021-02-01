d62e221ee9bfce2b0bc496a1efdb75c214b4935b
店小二 <75465319+scodery@users.noreply.github.com>
2021-02-01 12:08:53 +0800

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,8 +1,32 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-Vue.config.productionTip = false
+import toast from "components/common/toast";
+
+import FastClick from "fastclick";
+import VueLazyLoad from "vue-lazyload";
+
+Vue.config.productionTip = false;
+
+// 安装toast插件
+Vue.use(toast);
+
+// 事件总线
+Vue.prototype.$bus = new Vue();
+
+// 解决移动端300ms延迟
+FastClick.attach(document.body);
+
+// 使用懒加载的插件
+Vue.use(VueLazyLoad, {
+  // 占位图
+  loading: require("./assets/img/common/placeholder.png")
+});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  store,
+  render: h => h(App)
+}).$mount("#app");
